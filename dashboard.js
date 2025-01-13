255275c74c29ced086ce61fb4f4375f630927260
escobarssonn <ahmadridzki03@gmail.com>
2025-01-13 21:27:08 +0700

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -24,10 +24,10 @@ about:{ id:'about', css:'col-1-3', data:[
 },
 
 stats:{ id:'stats', css:'col-1-3',data:[
-{id:1,nama:'CLIENTS',isi:100,icon:'eye'},
+{id:1,nama:'CLIENT',isi:100,icon:'eye'},
 {id:1,nama:'PROJECTS',isi:100,icon:'camera'},
 {id:1,nama:'POSITIVE FEEDBACKS',isi:100,icon:'geo'},
-{id:1,nama:'Wallah',isi:100,icon:'pen'},
+{id:1,nama:'UPPERCUT',isi:100,icon:'pen'},
 ],
 },
 
